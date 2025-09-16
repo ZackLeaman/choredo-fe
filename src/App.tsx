5be8eb81a948eb7023c30f363e16212920c5d10a
Zackery Leaman <zleaman3@gmail.com>
2025-09-15 21:45:52 -0500

    Add: clerk to FE

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,19 @@ import { useSelector } from "react-redux";
 import { selectUser } from "@/slices";
 import AuthedRoutes from "@/routes/authedRoutes.component";
 import UnauthedRoutes from "@/routes/unauthedRoutes.component";
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 function App() {
   const user = useSelector(selectUser);
+
+  return <header>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+  </header>
 
   if (user && user.aud === 'authenticated' && user.id) {
     return <AuthedRoutes />
--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,22 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "@/store/store.ts";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <BrowserRouter basename="/choredo-fe">
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ClerkProvider>
   </BrowserRouter>
   // </StrictMode>,
 );
